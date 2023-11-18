0072c339712f4cb783d1dbf45d0c6a9c3c931478
Romаn Mоvchan <movchan@gmail.com>
2023-11-18 11:38:03 +0200

    schedule modal, close #13

---
+++ main.js
@@ -0,0 +1,19 @@
+const modal = document.getElementById("customModal");
+
+const button = document.getElementById("modalFormOpen");
+
+const close = document.getElementsByClassName("close")[0];
+
+button.onclick = function() {
+    modal.style.display = "block";
+}
+
+close.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
